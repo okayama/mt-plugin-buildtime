2c058e56b747f438d9f338704cb052767f90a5e0
okayama <kenmin.okayama@gmail.com>
2011-07-26 14:54:28 +0900

    Update for readability and error in listing screen in Movable Type 5.1

--- plugins/BuildTime/BuildTime.pl
+++ plugins/BuildTime/BuildTime.pl
@@ -1,46 +1,41 @@
 package MT::Plugin::BuildTime;
 use strict;
-
-use MT::Plugin;
 use MT;
+use MT::Plugin;
+use base qw( MT::Plugin );
 
 use Time::HiRes qw( gettimeofday );
 
-use base qw( MT::Plugin );
-
-###################################### Init Plugin #####################################
-
-my $plugin = MT::Plugin::BuildTime->new({
+my $plugin = MT::Plugin::BuildTime->new( {
     name => 'BuildTime',
     key => 'buildtime',
     id => 'BuildTime',
     version => '1.00',
     author_name => 'okayama',
     author_link => 'http://weeeblog.net/',
-    doc_link => 'http://weeeblog.net/logs/2008/05/24_1928.php',
+    doc_link => 'http://weeeblog.net/blogs/2008/05/24_1928.php',
     description => '<MT_TRANS phrase=\'_PLUGIN_DESCRIPTION\'>',
     l10n_class => 'BuildTime::L10N',
-});
+} );
 
-MT->add_plugin($plugin);
+MT->add_plugin( $plugin );
 
 sub init_registry {
     my $plugin = shift;
-    $plugin->registry({
+    $plugin->registry( {
         callbacks => {
             'MT::App::CMS::init_app'
                 => \&_init_app,
             'MT::App::CMS::take_down'
                 => \&_take_down,
         },
-   });
+   } );
 }
 
-####################################### Callbacks ######################################
-
 sub _init_app {
-    my ($cb, $app) = @_;
-    my ($epoch, $micro_seconds) = gettimeofday;
+    my ( $cb, $app ) = @_;
+    return if $app->mode eq 'filtered_list';
+    my ( $epoch, $micro_seconds ) = gettimeofday;
     my $from = {};
     $from = { epoch => $epoch,
               micro_seconds => $micro_seconds,
@@ -49,21 +44,22 @@ sub _init_app {
 }
 
 sub _take_down {
-    my ($cb, $app) = @_;
-    my ($epoch, $micro_seconds) = gettimeofday;
+    my ( $cb, $app ) = @_;
+    return if $app->mode eq 'filtered_list';
+    my ( $epoch, $micro_seconds ) = gettimeofday;
     my $to = {};
     $to = { epoch => $epoch,
             micro_seconds => $micro_seconds,
           };
-    my $from = $plugin->get_config_value('from');
-    my ($min, $micro);
-    $min = $to->{epoch} - $from->{epoch};
-    $micro = $to->{micro_seconds} - $from->{micro_seconds};
-    if ($micro < 0) {
+    my $from = $plugin->get_config_value( 'from' );
+    my ( $min, $micro );
+    $min = $to->{ epoch } - $from->{ epoch };
+    $micro = $to->{ micro_seconds } - $from->{ micro_seconds };
+    if ( $micro < 0 ) {
         $min--;
         $micro += 1000000;
     }
-    $app->print(sprintf("%d.%06d", $min ,$micro));
+    $app->print( sprintf( "%d.%06d", $min ,$micro ) );
 }
 
 1;
